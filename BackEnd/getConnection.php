429c3433dc73a16c708ae1ccc375e72c3984fa24
Emon(2017831003) <43314141+emon-swe-sust@users.noreply.github.com>
2019-09-28 12:34:05 +0600

    GetConnection Method completed.

--- BackEnd/getConnection.php
+++ BackEnd/getConnection.php
@@ -1,4 +1,3 @@
-
 <?php
 	
 	class Connection{
@@ -9,6 +8,19 @@
 			$username = "root";
 			$password = "";
 			$DB_NAME = "Tracker";
+
+			/**
+			  *Create connection and finally added database 
+			  *if we want to add database then we have to pass db name as last parameter to mysqli method
+			 **/
+
+			$conn = new mysqli($servername,$username,$password,$DB_NAME);
+
+			if($conn->connect_error){
+				die("connection failed: ". $conn->connect_error);
+			}
+
+			return $conn;
 		}
 	}
 ?>
