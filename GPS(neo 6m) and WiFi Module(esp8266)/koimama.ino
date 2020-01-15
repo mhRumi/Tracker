4cb5783198b7e5a450cb1b1308dcc2976dc0e6a8
Emon(2017831003) <43314141+emon-swe-sust@users.noreply.github.com>
2020-01-15 22:13:39 +0600

    Sending Latitude and Longitude to the Server

--- GPS(neo 6m) and WiFi Module(esp8266)/koimama.ino
+++ GPS(neo 6m) and WiFi Module(esp8266)/koimama.ino
@@ -21,13 +21,13 @@ SoftwareSerial ss(4,5); // for connect GPS
 const char* ssid = "DangerZone";
 const char* password = "nopassword";
 
-const char* host = "http://messmanagementtool.000webhostapp.com";
-String url = "/Tracker/DbConnect.php";
+const char* host = "**********";
+String url = "**********";
 
 float latitude , longitude;
 
 int year,month,date,hour,minute,second;
-String lat_str="44",lng_str="33",gppps="100";
+String lat_str="44",lng_str="33",gppps="100",id="*****";
 
 void setup()
 {
@@ -62,8 +62,35 @@ void loop()
         longitude = gps.location.lng();
         lng_str = String(longitude , 6);
         Serial.println(lat_str + " " + lng_str);
-        String loc = "GpsId=" + gppps + "&Latitude=" + lat_str + "&Longitude=" + lng_str;
+        String loc = "Id="+id+"&GpsId=" + gppps + "&Latitude=" + lat_str + "&Longitude=" + lng_str;
+        send(loc);
       }
+    }
+  }
+}
+
+void send(String loc)
+{
+    Serial.print(loc);
+  {
+    if(WiFi.status() == WL_CONNECTED){
+      HTTPClient http;
+      WiFiClient client;
+      const int httpport = 80;
+      if(!client.connect(host, httpport))
+      {
+        Serial.println("Failed");
+        return ;
+      }
+      Serial.println("Requesting");
+      String address = host + url;
+
+      http.begin(address);
+      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+      http.POST(loc);
+      http.end();
+      
+      delay(100);
     }
   }
 }
